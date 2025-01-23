b2809043f5e0f30771a94125b579fccbb999261e
Sehyeong Jo <150581845+sehyeongjo@users.noreply.github.com>
2025-01-24 01:52:44 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const googleApiKey = 'AIzaSyBLREZHOUXCRv2aiwIr_zZyyNT1_x9hDac';
-export const googleSheetDocId = '18z2nZGJR8jQjjoE2BWlH2HgpnVRTcVyvjYbKETnc-g0';
+export const googleApiKey = 'AIzaSyDWUJDd0aK4GSCO33ajMxUXDjMGTMl3T_M';
+export const googleSheetDocId = '1qh3terGxExsafSp2eznjhAXMON-5SP9Uy4QjkQd9a_k';
